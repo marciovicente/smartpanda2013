54964b3eb6891898828cf6a79058c0faccb2ef2c
Marcio Vicente <marciovicente.filho@gmail.com>
2013-04-24 15:11:04 -0300

    Ending main page

--- main.php
+++ main.php
@@ -10,57 +10,94 @@
 	<meta charset="UTF-8">
 	<title>Smartpanda - O shopping na palma da mão</title>
 	<link rel="stylesheet" href="<?php echo $server;?>css/index.css">
-	<link rel="stylesheet" href="<?php echo $server;?>css/bootstrap.min.css">
+	
 	<link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
 	<link rel="shortcut icon" type="image/x-icon" href="<?php echo $server;?>images/favicon.ico">
 </head>
 <body class="notLogged">
 	<div id="all" class="notLogged">
-		<div class="container">
-			<div class="central">
-				
-				<header id="headerMain">
+		<div class="container notLogged">
+
+			<header class="header">
+				<div id="headerMain">
+					
 					<a href="<?php echo $server ?>"><div id="logo"><span>O shopping na palma da mão</span></div></a>
 
-					<a class="btn-auth btn-facebook large" href="#">
-					    Entre com <b>Facebook</b>
-					</a>
-				</header>
-
-				<div class="banner">
+					<div class="auth">
+						<a class="btn-auth btn-facebook large" href="#"> Entre com <b>Facebook</b> </a>
+						<a class="btn-auth btn-lojista large">Lojista</a>
+					</div>
+				</div>
+			</header>
+			<div class="banner">
+				<div class="central">
 					<h2>Encontre tudo o que você precisa</h2>
 					<h3>com o shopping na palma da mão</h3>
 					<h3>(ou na tela do computador)</h3>
 					<button class="getButton"><span>Instale agora mesmo</span> <div class="androidIcon"></div> <h6>Versão 2.1 no Google Play</h6> </button>
 					<button class="getButton apple"><span>Instale agora mesmo</span>  <div class="appleIcon"></div></button>
 				</div>
+			</div>
 
-				<div class="banner second">
+			<div class="banner second">
+				<div class="central">
 					<div class="leftBanner">
-						<h2>Em qualquer lugar</h2>
-						<h3>De qualquer dispositivo</h3>
+						<h2>Em qualquer lugar,</h2>
+						<h3>de qualquer dispositivo</h3>
 						<button class="getButton"><span>Instale agora mesmo</span> <div class="androidIcon"></div> <h6>Versão 2.1 no Google Play</h6> </button>
 						<button class="getButton apple"><span>Instale agora mesmo</span>  <div class="appleIcon"></div></button>
 
 					</div>
-					<div class="devices"></div>
-					<!-- <h3>(ou na tela do computador)</h3> -->
 				</div>
-				<!-- <div id="leftMain">
+			</div>
+
+			<div class="central">
+				<h1>Como Funciona</h1>
+				<div class="iconsBio">
+					<div class="exp smartphone">
+						<h5>Instale o aplicativo em seu smartphone ou acesse pelo site</h5>	
+					</div>
+
+					<div class="exp pin">
+						<h5>Localize o shopping mais próximo ou de sua preferência</h5>	
+					</div>
+
+					<div class="exp deal">
+						<h5>Encontre os melhores anúncios para seu perfil</h5>	
+					</div>
+
+					<div class="exp cart">
+						<h5>Só sair para as compras!</h5>	
+					</div>
+
 					
 				</div>
 				
-				<div id="rightMain">
-					<h3>Baixe agora mesmo no seu smartphone</h3>
-					<div id="galaxy">
-						<a href=""><img src="images/gplay.png" alt="Google play icon"></a>
-					</div>
-					<div id="iphone">
-						<h2>BREVE!</h2>
-					</div>
-					
-				</div> -->
+				<div class="algorithms">
+					<h3>Através do Facebook entendemos o seu perfil e capturamos seus interesses</h3>
+					<h3>O algoritmo inteligente do Smartpanda é capaz de identificar apenas o que é de seu interesse</h3>
+					<h3>Você aproveita todos anúncios que o Smartpanda preparou pra você! </h3>
+				</div>
+				
+				
+	
 			</div>
+			
+
+			<div class="download">
+				<div class="central main">
+					<div class="textDownload">
+						Faça o download agora mesmo 
+						<br> 
+						<a href=""><div class="os android"></div>Android</a> 
+						<a href=""><div class="os ios"></div>iPhone</a>
+					</div>
+					<span>ou</span> 
+
+					<a class="btn-auth btn-facebook large" href="#"> Entre com <b>Facebook</b> </a>
+				</div>
+			</div>
+
 		</div>
 	</div> <!-- all -->
 
